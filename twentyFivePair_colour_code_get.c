26e0a901c14b5b8f9a8fae429c4d2ea205ee05a8
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 17:38:16 +0530

    Update twentyFivePair_colour_code_get.c
    resolved build error

--- twentyFivePair_colour_code_get.c
+++ twentyFivePair_colour_code_get.c
@@ -8,9 +8,9 @@ ColorPair GetColorFromPairNumber(int pairNumber)
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
     colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
 
