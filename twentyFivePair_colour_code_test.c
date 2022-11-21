239bedbf36d1efaeb5ba7f79fa11516b0764f648
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 17:13:23 +0530

    Create twentyFivePair_colour_code_test.c
    added test functions and its corresponding header files

---
+++ twentyFivePair_colour_code_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <assert.h>
+#include "twentyFivePair_colour_code_config.h"
+#include "twentyFivePair_colour_code_util.h"
+#include "twentyFivePair_colour_code_get.h"
+#include "twentyFivePair_colour_code_test.h"
+
+//function to check whether getting the number from pair is working fine.
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+//function to check whether getting the pair from number is working fine.
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
