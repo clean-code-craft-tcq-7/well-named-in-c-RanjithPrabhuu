bb7e9266fcb8bd7b2758fa97e89f0b7705477bf3
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 16:44:32 +0530

    twentyFivePair_colour_code_config.c
    removed and added the variables used in the 25 pair colour code pairing from the main file.

--- twentyFivePair_colour_code_config.c
+++ twentyFivePair_colour_code_config.c
@@ -1,1 +1,23 @@
+//array pointer to hold the names of the major colours
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
 
+//integer variable to hold the total number of major colours
+int numberOfMajorColors =
+    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+//array pointer to hold the names of the minor colours
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+//integer variable to hold the total number of minor colours
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+//structure which holds the major and minor colour of the colour code pairing
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
