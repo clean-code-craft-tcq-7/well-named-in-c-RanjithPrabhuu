76f5bbabe7e97b4c120b23e8f9659e3a17a3b361
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 18:04:23 +0530

    Update twentyFivePair_colour_code_util.c
    updated the file

--- twentyFivePair_colour_code_util.c
+++ twentyFivePair_colour_code_util.c
@@ -4,7 +4,7 @@
 #include "twentyFivePair_colour_code_util.h"
 
 //util fucntion to print colour pairs identified in strings.
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+void ColorPairToString(const ColorPair colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
