971a11e86e0a7e521dc4c350faad7ab3e5d3620d
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 19:16:12 +0530

    Update twentyFivePair_colour_code_util.h

--- twentyFivePair_colour_code_util.h
+++ twentyFivePair_colour_code_util.h
@@ -1,5 +1,3 @@
-//extern of the structure created for major and minor colours
-extern ColorPair colorPair;
 
 // declaration of print util function for displaying the colour pairs in strings.
 void ColorPairToString(const ColorPair colorPair, char* buffer);
