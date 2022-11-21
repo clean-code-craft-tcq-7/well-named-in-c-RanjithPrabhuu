eae94e861f27bc886e53d17949b3101baaa92256
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 16:57:38 +0530

    Update twentyFivePair_colour_code_util.c
    included the util.h header file

--- twentyFivePair_colour_code_util.c
+++ twentyFivePair_colour_code_util.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "twentyFivePair_colour_code_config.h"
+#include "twentyFivePair_colour_code_util.h"
 
 //util fucntion to print colour pairs identified in strings.
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
