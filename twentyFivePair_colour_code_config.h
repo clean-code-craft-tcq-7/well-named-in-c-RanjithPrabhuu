e20981e3ac2be33a62fe2f2af7c55247b3b0e7d0
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 16:39:30 +0530

    updated the twentyFivePair_colour_code_config.h
    included the enum and macro from the main file

--- twentyFivePair_colour_code_config.h
+++ twentyFivePair_colour_code_config.h
@@ -1,1 +1,6 @@
+const int MAX_COLORPAIR_NAME_CHARS = 16
 
+//enum which consists the major colours in the 25 pair colour code table.
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET}; 
+//enum which consists the minor colours in the 25 pair colour code table.
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
