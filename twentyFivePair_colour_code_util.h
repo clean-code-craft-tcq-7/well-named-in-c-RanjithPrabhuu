19e4c580bea9e31669f10d8d58565e7ca53855f6
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 17:54:49 +0530

    Update twentyFivePair_colour_code_util.h
    removed header files

--- twentyFivePair_colour_code_util.h
+++ twentyFivePair_colour_code_util.h
@@ -1,4 +1,3 @@
-#include "twentyFivePair_colour_code_get.h"
 
 // declaration of print util function for displaying the colour pairs in strings.
 void ColorPairToString(const ColorPair colorPair, char* buffer);
