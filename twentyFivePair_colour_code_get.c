2ada287a6dcf8a86852f49be39b9d6d609673f40
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-21 17:00:37 +0530

    Create twentyFivePair_colour_code_get.c
    added the definition for get functions

---
+++ twentyFivePair_colour_code_get.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "twentyFivePair_colour_code_config.h"
+#include "twentyFivePair_colour_code_get.h"
+
+//function used to get the colour by giving the input of the pair number
+ColorPair GetColorFromPairNumber(int pairNumber) 
+{
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+//function used to get the pair number by giving the input of the colour
+int GetPairNumberFromColor(const ColorPair* colorPair) 
+{
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
