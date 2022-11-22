5aa8caa117a3d05f166952c7043d1ec4611999d8
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-22 08:42:25 +0530

    Update twentyFivePair_colour_code_get.h

--- twentyFivePair_colour_code_get.h
+++ twentyFivePair_colour_code_get.h
@@ -5,8 +5,8 @@ typedef struct {
 } ColorPair;
 
 // declaration of print util function for displaying the colour pairs in strings.
-void ColorPairToString(const ColorPair colorPair, char* buffer);
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
 // function declaration to get the colour from the pair number.
 ColorPair GetColorFromPairNumber(int pairNumber);
 // function declaration to get the pair number from the colour.
-int GetPairNumberFromColor(const ColorPair colorPair);
+int GetPairNumberFromColor(const ColorPair* colorPair);
