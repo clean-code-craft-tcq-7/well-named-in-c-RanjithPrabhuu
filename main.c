4b7d64b9e982677cb0da0fe0cc05d5011f902bc2
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-11-22 09:05:28 +0530

    Update main.c
    added display functions

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 #include "twentyFivePair_colour_code_test.h"
 #include "twentyFivePair_colour_code_display.h"
 
-int main() {
+int main() 
+{
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+    
+    twentyFivePairColourCodeDisplay();
 
     return 0;
 }
